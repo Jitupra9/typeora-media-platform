e9b79b20ea1b85ea78877b0284af2264cd06a4af
Jitu Pradhan <jitupra73@gmail.com>
2025-05-30 18:03:21 +0530

    change in save component

--- client/src/pages/saved/Saved.jsx
+++ client/src/pages/saved/Saved.jsx
@@ -1,12 +1,21 @@
-import React from "react";
-import { Search, ChevronDown, Ellipsis } from "lucide-react";
+import React, { useRef } from "react";
+import { Search, ChevronDown, Ellipsis, Info } from "lucide-react";
 function Saved() {
+  const dateInputRef = useRef(null);
+  const dateInputReftwo = useRef(null);
+  const timeInputRef = useRef(null);
+  const timeInputReftwo = useRef(null);
+
+  const handleIconClick = (dateref) => {
+    if (dateref.current) {
+      dateref.current.showPicker();
+    }
+  };
   return (
     <div>
       {" "}
       <div className=" filter-details text-gray-600 dark:text-gray-200  bg-white dark:bg-gray-900 bg-opacity-90 p-5 rounded-md ">
         <div className=" filters flex justify-between ">
-          {/* */}
           <div>
             <h3 className=" text-3xl font-bold mb-3">Welcome Back</h3>
             <p>Get your latest for the last 7 days</p>
@@ -43,8 +52,14 @@ function Saved() {
                 type="date"
                 name=""
                 id=""
+                ref={dateInputReftwo}
               />
-              <ChevronDown className=" w-5 h-5" />
+              <ChevronDown
+                className=" w-5 h-5 cursor-pointer"
+                onClick={() => {
+                  handleIconClick(dateInputReftwo);
+                }}
+              />
             </div>
           </div>
           <div>
@@ -55,8 +70,14 @@ function Saved() {
                 type="date"
                 name=""
                 id=""
+                ref={dateInputRef}
               />
-              <ChevronDown className=" w-5 h-5" />
+              <ChevronDown
+                className=" w-5 h-5 cursor-pointer"
+                onClick={() => {
+                  handleIconClick(dateInputRef);
+                }}
+              />
             </div>
           </div>
           <div>
@@ -64,11 +85,17 @@ function Saved() {
             <div className="bg-gray-200 dark:bg-gray-800  px-2 py-2 rounded-md flex overflow-hidden">
               <input
                 className=" px-3 date_icon_off  bg-transparent outline-none "
-                type="date"
+                type="time"
                 name=""
-                id=""
+                id="time"
+                ref={timeInputRef}
               />
-              <ChevronDown className=" w-5 h-5" />
+              <ChevronDown
+                className=" w-5 h-5 cursor-pointer"
+                onClick={() => {
+                  handleIconClick(timeInputRef);
+                }}
+              />
             </div>
           </div>
           <div>
@@ -76,21 +103,41 @@ function Saved() {
             <div className="bg-gray-200 dark:bg-gray-800  px-2 py-2 rounded-md flex overflow-hidden">
               <input
                 className=" px-3 date_icon_off  bg-transparent outline-none "
-                type="date"
+                type="time"
                 name=""
-                id=""
+                id="time"
+                ref={timeInputReftwo}
               />
-              <ChevronDown className=" w-5 h-5" />
+              <ChevronDown
+                className=" w-5 h-5 cursor-pointer"
+                onClick={() => {
+                  handleIconClick(timeInputReftwo);
+                }}
+              />
             </div>
           </div>
         </div>
       </div>
-      <div className=" content mt-20 text-gray-600 dark:text-gray-200  bg-white dark:bg-gray-900 bg-opacity-90 p-5 rounded-md ">
-        <div>
-          <ul className=" flex gap-x-10">
-            <li>Article</li>
+      <div className=" content mt-20 text-gray-600 dark:text-gray-200">
+        <div className=" my-5  ">
+          <ul className=" flex gap-x-5  *:px-6 *:py-2  *:rounded-md ">
+            <li className="bg-white  dark:bg-gray-800">Article</li>
             <li>Videos</li>
           </ul>
+        </div>
+        <div className=" flex flex-col w-full sm:flex-row justify-between *:bg-white *:dark:bg-gray-900 *:bg-opacity-90 *:p-5 *:rounded-md ">
+          <div className="content w-[62%]">
+            <div className="header flex justify-between">
+              <h1>Available Cars</h1>
+              <p>View All</p>
+            </div>
+          </div>
+          <div className="resent-activity w-[34%] ">
+            <div className="header flex justify-between">
+              <p>Recent Activity</p>
+              <Info />
+            </div>
+          </div>
         </div>
       </div>
     </div>
