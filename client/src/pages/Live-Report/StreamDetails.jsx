2b73b37e8a295e7dfea9c529a15cc056b5c5ea31
Jitu Pradhan <jitupra73@gmail.com>
2025-05-30 21:56:32 +0530

    change in video section

--- client/src/pages/Live-Report/StreamDetails.jsx
+++ client/src/pages/Live-Report/StreamDetails.jsx
@@ -1,11 +1,11 @@
 import React from "react";
-import LiveStream from "./Video";
+import Video from "./Video";
 import ContentSlider from "../../component/Layout/ContentSlider";
 function StreamDetails() {
   return (
     <div className="flex gap-1">
       <div className="w-full lg:w-[60%]">
-        <LiveStream />
+        <Video />
       </div>
       <div className=" hidden lg:block lg:w-[39%]">
         <ContentSlider />
